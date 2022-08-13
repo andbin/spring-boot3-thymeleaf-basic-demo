2d3b278f2fa7c0bbc4e3e372fead4e5b2346c913
andbin <andbin@users.noreply.github.com>
2022-08-13 16:45:01 +0200

    Add server information displayed on the home page

--- src/main/java/dev/andbin/springboot/demo/service/AppInfoService.java
+++ src/main/java/dev/andbin/springboot/demo/service/AppInfoService.java
@@ -5,26 +5,32 @@ SPDX-License-Identifier: MIT
 
 package dev.andbin.springboot.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.core.SpringVersion;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.Thymeleaf;
 
 import dev.andbin.springboot.demo.model.AppInfo;
+import jakarta.servlet.ServletContext;
 
 @Service
 public class AppInfoService {
+    @Autowired
+    private ServletContext servletContext;
+
     public AppInfo getAppInfo() {
         AppInfo appInfo = new AppInfo();
         appInfo.setJavaRuntimeVersion(getJavaRuntimeVersion());
         appInfo.setSpringVersion(getSpringVersion());
         appInfo.setSpringBootVersion(getSpringBootVersion());
         appInfo.setThymeleafVersion(getThymeleafVersion());
+        appInfo.setServerInfo(getServerInfo());
         return appInfo;
     }
 
     private String getJavaRuntimeVersion() {
-        return Runtime.version().toString();
+        return Runtime.version().toString();   // Java 9+
     }
 
     private String getSpringVersion() {
@@ -37,5 +43,9 @@ public class AppInfoService {
 
     private String getThymeleafVersion() {
         return Thymeleaf.getVersion();
+    }
+
+    private String getServerInfo() {
+        return servletContext.getServerInfo();
     }
 }
